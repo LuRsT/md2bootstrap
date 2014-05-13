d3a438595e279278bd03429ed6cfba5f7b380cdf
Gil Gonçalves <lursty@gmail.com>
2014-05-13 17:05:39 +0100

    Added setup.py to be able to install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='md2bootstrap',
+    version='0.1',
+    packages=[],
+    license='MIT',
+    long_description=open('README.md').read(),
+    scripts=['md2bootstrap/bin/md2bootstrap'],
+    install_requires=[]
+)
